e6adceec9dee31b0ce8f4d7a56f4fee09167642d
N-Echelibe <nathanielechelibe5@gmail.com>
2025-03-18 20:13:09 +0100

    Integrated supabase
    Integrated supabase for storing images and included environment variables

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import multer from "multer";
 // ☝️ is used to handle files cuz express can't
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+const supabase = createClient(
+  process.env.SUPERBASE_URL,
+  process.env.SUPERBASE_KEY
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniquePrefix + "-" +  file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -33,11 +34,20 @@ app.get("/modify", (req, res) => {
   res.render("modify.ejs");
 });
 
-app.post("/modify", upload.single("thumbnail"), (req, res) => {
-  const title = req.body.title;
-  const imagePath = req.file;
-  console.log(title);
-  console.log(imagePath);
+app.post("/modify", upload.single("thumbnail"), async (req, res) => {
+  try {
+    if (req.file) {
+      const filename = Date.now() + "-" + req.file.originalname;
+      const { data, error } = await supabase.storage
+        .from("uploads")
+        .upload(`thumbnail/${filename}`, req.file.buffer);
+      if (error) throw error;
+      const imageURL = `${process.env.SUPERBASE_URL}/storage/v1/object/public/uploads/${filename}`;
+      console.log(imageURL);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
