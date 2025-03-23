d7f32d0ec0fade63482e798819560e79b2433afc
N-Echelibe <nathanielechelibe5@gmail.com>
2025-03-23 20:41:44 +0100

    Added dynamic rendering of Posts
    Added dynamic rendering of posts to homepage and individual posts link. Updated supabase client. Added marked npm package to convert markdown content to HTML. Configured refresh token to keep users logged in. Added a logout function.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import multer from "multer";
 import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 import cookieParser from "cookie-parser";
+import { marked, Marked } from "marked";
+// ☝️ is used to convert markdown content to HTML
 
 dotenv.config();
 
@@ -13,14 +15,17 @@ const port = process.env.PORT || 5000;
 
 const supabase = createClient(
   process.env.SUPERBASE_URL,
-  process.env.SUPERBASE_KEY,
+  process.env.SUPERBASE_KEY
 );
 const supabaseWithAuth = (req) => {
-  return createClient(
-    process.env.SUPERBASE_URL,
-    process.env.SUPERBASE_KEY,
-    { global: { headers: { Authorization: `Bearer ${req.cookies.session_token}` } } }
-  );
+  if (req.cookies.session_token) {
+    return createClient(process.env.SUPERBASE_URL, process.env.SUPERBASE_KEY, {
+      global: {
+        headers: { Authorization: `Bearer ${req.cookies.session_token}` },
+      },
+    });
+  }
+  return createClient(process.env.SUPERBASE_URL, process.env.SUPERBASE_KEY);
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,8 +36,23 @@ app.use(cookieParser());
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const authenticate = async (req, res, next) => {
-  const token = req.cookies.session_token;
-  if (!token) {
+  const supabaseAuth = supabaseWithAuth(req);
+  let token = req.cookies.session_token;
+  const refresh_token = req.cookies.refresh_token;
+
+  if (!token && refresh_token) {
+    const { data, error } = await supabaseAuth.auth.refreshSession({
+      refresh_token: refresh_token,
+    });
+    if (data?.session) {
+      token = data.session.access_token;
+      res.cookie("session_token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 86400000,
+      });
+    }
+  } else if (!token) {
     req.user = null;
     return next();
   }
@@ -51,11 +71,32 @@ const authenticate = async (req, res, next) => {
 };
 
 app.get("/", authenticate, async (req, res) => {
-  res.render("home.ejs", { user: req.user });
+  const supabaseAuth = supabaseWithAuth(req);
+  try {
+    const { data, error } = await supabaseAuth.from("userPosts").select("*");
+    if (error) throw error;
+    res.render("home.ejs", { user: req.user, posts: data });
+  } catch (err) {
+    console.log(err);
+  }
+  // res.render("home.ejs", { user: req.user });
 });
 
-app.get("/post", (req, res) => {
-  res.render("post.ejs");
+app.get("/post/:id", async (req, res) => {
+  const supabaseAuth = supabaseWithAuth(req);
+  const id = req.params.id;
+  try {
+    const { data, error } = await supabaseAuth
+      .from("userPosts")
+      .select("*")
+      .eq("post_id", id)
+      .single();
+    if (error) throw error;
+    const content = marked(data.content);
+    res.render("post.ejs", { post: data, content: content });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/login", (req, res) => {
@@ -66,33 +107,46 @@ app.post("/login", async (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(400).json("Username or Password not specified");
   }
+  const supabaseAuth = supabaseWithAuth(req);
   const email = req.body.email;
   const password = req.body.password;
   try {
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabaseAuth.auth.signInWithPassword({
       email,
       password,
     });
-    if (error) throw error;
-    res.cookie("session_token", data.session.access_token, {
-      httpOnly: true, //prevents client-side js from accessing the cookie
-      secure: true, //set true if using https
-      maxAge: 7 * 86400000, // expiration time in ms (7days)
-    });
-    res.json({ redirect: "/" });
+    if (error) {
+      throw error;
+    } else {
+      res.cookie("session_token", data.session.access_token, {
+        httpOnly: true, //prevents client-side js from accessing the cookie
+        secure: true, //set true if using https
+        maxAge: 7 * 86400000, // expiration time in ms (7days)
+      });
+      res.cookie("refresh_token", data.session.refresh_token, {
+        httpOnly: true, //prevents client-side js from accessing the cookie
+        secure: true, //set true if using https
+        maxAge: 30 * 86400000, // expiration time in ms (7days)
+      });
+      res.json({ redirect: "/" });
+    }
+
     // When you call res.json(), res.send(), or res.end(), the response is finalized, meaning no more headers (like cookies) can be added afterward.
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
+app.get("/signup", async (req, res) => {
+  res.render("signup.ejs");
+})
+
 app.get("/cookies", authenticate, async (req, res) => {
   console.log(req.cookies.session_token);
   const supabaseAuth = supabaseWithAuth(req);
   const { data, error } = await supabaseAuth.rpc("get_current_role");
   console.log(data);
-  // const { data, error } = await supabase.rpc("get_current_role");
-  // console.log(data);
+  console.log(req.cookies.refresh_token);
 });
 
 app.get("/modify", authenticate, (req, res) => {
@@ -137,8 +191,19 @@ app.post(
 );
 
 app.get("/profile", authenticate, async (req, res) => {
+  if (!req.user) return res.redirect("/login");
   res.render("profile.ejs");
-})
+});
+
+app.get("/logout", authenticate, async (req, res) => {
+  // res.clearCookie("session_token");
+  const supabaseAuth = supabaseWithAuth(req);
+  const { error } = await supabaseAuth.auth.signOut();
+  if (error) throw error;
+  res.clearCookie("session_token");
+  res.clearCookie("refresh_token");
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
